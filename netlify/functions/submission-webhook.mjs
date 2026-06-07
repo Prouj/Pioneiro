cd5c8388c85656373bbe6a137793dd95b661ceac
Leonardo Lima Cavalcante <leonardo123k@gmail.com>
2026-06-07 17:05:55 -0300

    add formSubmitted event handler

---
+++ netlify/functions/submission-webhook.mjs
@@ -0,0 +1,26 @@
+export default {
+    formSubmitted(event) {
+        const RESEND_API_KEY = process.env.RESEND_API_KEY;
+        const RESEND_EMAIL = process.env.RESEND_EMAIL;
+        const { name, email, message } = JSON.parse(event.body);
+        console.log("Received submission:", event);
+        fetch('https://api.resend.com/emails', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${RESEND_API_KEY}`
+            },
+            body: JSON.stringify({
+                from: RESEND_EMAIL,
+                to: email,
+                subject: `New submission from ${name}`,
+                html: `<p><strong>Name:</strong> ${name}</p><p><strong>Email:</strong> ${email}</p><p><strong>Message:</strong> ${message}</p>`
+            })
+        });
+    },
+
+    config: {
+        path: '/submission-webhook',
+        method: ['GET', 'POST']
+    }
+}

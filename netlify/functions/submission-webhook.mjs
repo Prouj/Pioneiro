039790c466fd7bd6474aa49ff74b95569ba0e9a3
Leonardo Lima Cavalcante <leonardo123k@gmail.com>
2026-06-07 18:42:19 -0300

    error logs

--- netlify/functions/submission-webhook.mjs
+++ netlify/functions/submission-webhook.mjs
@@ -21,8 +21,8 @@ export default {
         });
 
         if (!response.ok) {
-            console.error(`Failed to send email: ${response.statusText}`);
-            return new Response('Failed to send email', { status: response.status, statusText: response.statusText });
+            console.error(`Failed to send email: ${response.status} ${response.statusText} ${await response.json()}`);
+            return new Response('Failed to send email', { status: response.status, statusText: response.statusText, body: JSON.stringify(await response.json()) });
         }
 
         console.log("Email sent successfully - ", (await response.json()).id);
